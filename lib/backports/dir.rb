3be21eefd3cfece3d71b8a72e71451c1b00e69de
Jens Wille <jens.wille@uni-koeln.de>
2008-10-27 13:47:45 +0100

    Prevent multiple overrides. (v0.0.2)

--- lib/backports/dir.rb
+++ lib/backports/dir.rb
@@ -3,14 +3,14 @@ class Dir
   class << self
 
   if RUBY_RELEASE_DATE < '2007-02-15'
-    alias_method :_backports_original_brackets, :[]
+    unless method_defined?(:_backports_original_brackets)
+      alias_method :_backports_original_brackets, :[]
 
-    # r11748 -- can now take many patterns
-    def [](*args)
-      args.map { |arg| _backports_original_brackets(arg) }.flatten
+      # r11748 -- can now take many patterns
+      def [](*args)
+        args.map { |arg| _backports_original_brackets(arg) }.flatten
+      end
     end
-  end
-
   end
 
 end
